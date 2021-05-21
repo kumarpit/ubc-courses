d65cfa8badf6f15518d73b5811028e904ccdc694
kumarpit <kumar.arpit77@yahoo.com>
2021-05-21 16:09:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const canv = document.getElementById("gameCanvas")
 const ctx  = canv.getContext("2d")
 const BIRD_RADIUS = 12
-const PIPE_SPEED = 2
+const PIPE_SPEED = 3
 const PIPE_WIDTH = 30
 const MAX_FALL_SPEED = 10
 const GAP = 120
 const GRAVITY = 0.3
 const JUMP = 5
 const MIN_TOP_HEIGHT = 10
+const INTERVAL = 1500
 let pipes = []
+let score = 0
 
 //define classes
 class Bird{
@@ -24,7 +26,6 @@ class Bird{
 		ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
 		ctx.fill();
 		ctx.fillStyle = "blue"
-		ctx.fillRect(this.x, this.y, 1, 1)
 	}
 	fall(){
 		if(this.fallSpeed < MAX_FALL_SPEED){
@@ -42,10 +43,11 @@ class Bird{
 
 class Pipe{
 	constructor(){
-		this.top = Math.random() * canv.height / 2 + MIN_TOP_HEIGHT;
+		this.top = Math.random() * canv.height / 2 + MIN_TOP_HEIGHT
 		this.bottom = this.top + GAP
 		this.x = canv.width + PIPE_WIDTH
 		this.color = "white"
+		this.checked = false
 	}
 	drawPipe(){
 		ctx.fillStyle = this.color
@@ -57,15 +59,26 @@ class Pipe{
 	}
 	hits(bird){
 		if((bird.y - bird.r < this.top || bird.y + bird.r > this.bottom) && 
-		   (bird.x + bird.r > this.x && !(bird.x - bird.r > this.x + PIPE_WIDTH))){
+		   (bird.x + bird.r > this.x && !this.crossed(bird))){
+		   	this.checked = true
+			return true
+		}else if(this.crossed(bird) && !this.checked){ //scoring 
+			score++
+			this.checked = true
+			console.log(score)
+		}
+	}
+	crossed(bird){
+		if(bird.x - bird.r > this.x + PIPE_WIDTH){
 			return true
 		}
+
 	}
 }
 
 let lastTime = 0
 let deltaTime = 0
-let spawnInterval = 2000
+let spawnInterval = INTERVAL
 let spawnCounter = 0
 
 //main game loop
@@ -85,9 +98,9 @@ function update(time){
 	ctx.fillRect(0, 0, canv.width, canv.height)
 
 	//move the bird - gravity, jump
-	if(bird.y <= canv.height - bird.r){
+	if(bird.y + bird.r < canv.height){
 		bird.fall()
-	}
+	}else bird.y = canv.height - bird.r
 
 	//move the pipes, remove if they go beyond screen
 	// check colision between bird and pipes
@@ -101,6 +114,7 @@ function update(time){
 			pipes[i].color = "red"
 		} else pipes[i].color = "white"
 	}
+
 
 	//draw the pipes, bird
 	bird.drawBird()
