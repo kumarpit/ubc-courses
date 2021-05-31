0f7a167560d6ce2111fb68239a949959f6fb8513
kumarpit <kumar.arpit77@yahoo.com>
2021-06-01 04:17:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,64 +7,55 @@ from bs4 import BeautifulSoup
 import sys
 import pyperclip as pc
 
-def getCourses(dept):
-	get_url = "https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-department&dept="+dept
-	request_page = urlopen(get_url)
+def scrape(url):
+	request_page = urlopen(url)
 	page_html = request_page.read()
 	request_page.close()
-	html_soup = BeautifulSoup(page_html, "html.parser")
+	return page_html
 
-	table = html_soup.find("table", id="mainTable")
-
-	for tr in table.find_all("tr"):
+def printTable(table):
+	for tr in table:
 		td = tr.find_all("td")
 		row = [i.text for i in td]
 		print(row)
 
+def getCourses(dept):
+	get_url = "https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-department&dept="+dept
+	page_html = scrape(get_url)
+	html_soup = BeautifulSoup(page_html, "html.parser")
+	table = html_soup.find("table", id="mainTable")
+	printTable(table.find_all("tr"))
 	start()
 
 def getSections(dept, course):
 	get_url = "https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-course&dept="+dept+"&course="+course
-	request_page = urlopen(get_url)
-	page_html = request_page.read()
-	request_page.close()
+	page_html = scrape(get_url)
 	html_soup = BeautifulSoup(page_html, "html.parser")
-
 	table = html_soup.find("table", class_="section-summary")
 
 	for to_delete in table.find_all("td", class_="section-comments"):
 		to_delete.decompose()
 
-	for tr in table.find_all("tr"):
-		td = tr.find_all("td")
-		row = [i.text for i in td]
-		print(row)
-
+	printTable(table.find_all("tr"))
 	start()
 
 def getSeats(dept, course, section):
 	get_url = "https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-section&dept="+dept+"&course="+course+"&section="+section
-	request_page = urlopen(get_url)
-	page_html = request_page.read()
-	request_page.close()
+	page_html = scrape(get_url)
 	html_soup = BeautifulSoup(page_html, "html.parser")
-
 	strong_el = html_soup.find_all("strong")
+
 	for el in strong_el:
 		print(el.text)
 
 	pc.copy(get_url)
 	print("Link copied to clipboard")
-
 	start()
 
 def getAll():
 	get_url = "https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea"
-	request_page = urlopen(get_url)
-	page_html = request_page.read()
-	request_page.close()
+	page_html = scrape(get_url)
 	html_soup = BeautifulSoup(page_html, "html.parser")
-
 	table = html_soup.find("table", id="mainTable")
 
 	for tr in table.find_all("tr"):
@@ -77,11 +68,8 @@ def getAll():
 
 def getCurrTerm():
 	get_url = "https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea"
-	request_page = urlopen(get_url)
-	page_html = request_page.read()
-	request_page.close()
+	page_html = scrape(get_url)
 	html_soup = BeautifulSoup(page_html, "html.parser")
-
 	buttons = html_soup.find_all("button", class_="btn-primary")
 
 	for bt in buttons:
